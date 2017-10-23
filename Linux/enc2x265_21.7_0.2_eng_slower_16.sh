5d1a950a9adfc409cc094ebd1439edff65b982b8
Rayz Clowder <orayzo@gmail.com>
2017-10-23 18:11:38 +0800

    Update enc2x265_21.7_0.2_eng_slower_16.sh
    use mpdecimate to de-depulicate frames

--- Linux/enc2x265_21.7_0.2_eng_slower_16.sh
+++ Linux/enc2x265_21.7_0.2_eng_slower_16.sh
@@ -1,143 +1,161 @@
 #/bin/bash
-#written by @rzcat :)
 
-function compress () {
+function compress {
 
 	videoencq=21.7
 	audioencq=0.2
 	speedctl=slower
-	#tunectl=film #目前x265闲置
+	#tunectl=film #not using
 	MuxLang=eng
 	dar=16:9
 
 	input="$1"
-	outputdir=/home/rzcat/x265/output
-	#注意上面这行末尾没有斜杠
-	output=$(echo "$input"|sed -r 's/(.*)(\..*)/\1/g')
-	output=$(basename "$output")
+	outputdir='/path/to/outputdir'
+	output=$(echo "${input}"|sed -r 's/(.*)(\..*)/\1/g')
+	output=$(basename "${output}")
+	#timecodefile=`echo ${output}`_timecode.txt
+	#LOGFILE="${outputdir}/LOG_$(date +%Y.%m).log"
 
-	if [ $(ffmpeg-10bit -i "$input" 2>&1 | grep "Subtitle" | wc -l) -gt 0 ]; then
-		tail=\(x265_$(ffmpeg-10bit -i "$input" 2>&1 | grep "Audio" | wc -l)Aac_$(ffmpeg-10bit -i "$input" 2>&1 | grep "Subtitle" | wc -l)Sub.${videoencq}_${audioencq}.${speedctl}\)
+	AudioNum=$(ffmpeg-10bit -i "${input}" 2>&1 | grep "Audio" | wc -l)
+	SubNum=$(ffmpeg-10bit -i "${input}" 2>&1 | grep "Subtitle" | wc -l)
+
+	if [ ${SubNum} -gt 0 ]; then
+		tail=\(x265_${AudioNum}Aac_${SubNum}Sub.${videoencq}_${audioencq}.${speedctl}\)
 	else
-		tail=\(x265_$(ffmpeg-10bit -i "$input" 2>&1 | grep "Audio" | wc -l)Aac.${videoencq}_${audioencq}.${speedctl}\)
+		tail=\(x265_${AudioNum}Aac.${videoencq}_${audioencq}.${speedctl}\)
 	fi
 
+	#echo "$(date +"%Y.%m.%d %H:%M:%S") Start Encoding..." | tee -a ${LOGFILE}
 	echo
-	echo Input: $input
-	echo Output: $outputdir/$output.$tail.mkv
+	echo "Input: ${input}" #| tee -a ${LOGFILE}
+	echo "Output: ${outputdir}/${output}.${tail}.mkv" #| tee -a ${LOGFILE}
 	echo
 
-	if [ -f "$outputdir/$output.$tail.mkv" ]; then
-		echo "Warning: Output File Already Exists, nothing touched."
+	if [ -f "${outputdir}/${output}.${tail}.mkv" ]; then
+		echo "Warning: Output File Already Exists, nothing touched." #| tee -a ${LOGFILE}
 	else
-		if [ -f "$outputdir/$output.tmp.mp4" ]; then
-			rm -f "$outputdir/$output.tmp.mp4"
+		if [ -f "${outputdir}/${output}.tmp.mp4" ]; then
+			rm -f "${outputdir}/${output}.tmp.mp4"
 		fi
-		if [ ! -f "$outputdir/$output.done.mp4" ]; then
+		if [ ! -f "${outputdir}/${output}.done.mp4" ]; then
+			#echo "$(date +"%Y.%m.%d %H:%M:%S") Start of Video Encoding..." | tee -a ${LOGFILE}
 			ffmpeg-10bit -hide_banner \
-				-i "$input" \
+				-i "${input}" \
 				-map 0:v:0 \
 				-map_chapters 0 \
-				-aspect $dar \
+				-vf mpdecimate \
+				-vsync vfr \
+				-aspect ${dar} \
 				-vcodec libx265 \
 				-pix_fmt yuv420p10le \
-				-preset $speedctl \
-				-crf $videoencq \
+				-preset ${speedctl} \
+				-crf ${videoencq} \
 				-an \
 				-sn \
-				"$outputdir/$output.tmp.mp4"&&mv "$outputdir/$output.tmp.mp4" "$outputdir/$output.done.mp4"
+				"${outputdir}/${output}.tmp.mp4"&&mv "${outputdir}/${output}.tmp.mp4" "${outputdir}/${output}.done.mp4"
 		fi
-		for ((mapindex=0; mapindex<$(ffmpeg-10bit -i "$input" 2>&1 | grep "Audio" | wc -l); ++mapindex))
+		#echo "$(date +"%Y.%m.%d %H:%M:%S") End of Video Encoding, Start of Audio Encoding..." | tee -a ${LOGFILE}
+		for ((mapindex=0; mapindex<${AudioNum}; ++mapindex))
 		do
-			if [ -f "$outputdir/$output.tmp.m4a" ]; then
-				rm -f "$outputdir/$output.tmp.m4a"
+			if [ -f "${outputdir}/${output}.tmp.m4a" ]; then
+				rm -f "${outputdir}/${output}.tmp.m4a"
 			fi
-			if [ ! -f "$outputdir/$output.a$mapindex.m4a" ]; then
+			if [ ! -f "${outputdir}/${output}.a${mapindex}.m4a" ]; then
 				ffmpeg-10bit -hide_banner \
 					-v quiet \
-					-i "$input" \
+					-i "${input}" \
 					-vn -sn -dn \
-					-map 0:a:$mapindex \
+					-map 0:a:${mapindex} \
 					-acodec pcm_f32le \
 					-f wav pipe:|neroAacEnc \
 					-ignorelength \
-					-q $audioencq \
-					-if - -of "$outputdir/$output.tmp.m4a"&&mv "$outputdir/$output.tmp.m4a" "$outputdir/$output.a$mapindex.m4a"
+					-q ${audioencq} \
+					-if - -of "${outputdir}/${output}.tmp.m4a"&&mv "${outputdir}/${output}.tmp.m4a" "${outputdir}/${output}.a${mapindex}.m4a"
 			fi
-		done		
-		cmd_mux_audio=$(for ((mapindex=0; mapindex<$(ffmpeg-10bit -i "$input" 2>&1 | grep "Audio" | wc -l); ++mapindex)); do \
-			printf ' --no-track-tags --no-chapters --language 0:'$MuxLang' ( '$outputdir'/'$output'.a'$mapindex'.m4a )'; done)
-		cmd_mux_tail='--track-order 0:0'$(for ((mapindex=0; mapindex<$(ffmpeg-10bit -i "$input" 2>&1 | grep "Audio" | wc -l); ++mapindex)); do \
-			printf ','$(($mapindex+1))':0'; done)
-		if [ $(ffmpeg-10bit -i "$input" 2>&1 | grep "Subtitle" | wc -l) -gt 0 ]; then
-			if [ -f "$outputdir/$output.subtmp.mkv" ]; then
-				rm -f "$outputdir/$output.subtmp.mkv"
+		done
+		#echo "$(date +"%Y.%m.%d %H:%M:%S") End of Audio Encoding..." | tee -a ${LOGFILE}
+		cmd_mux_audio=$(for ((mapindex=0; mapindex<${AudioNum}; ++mapindex))
+									do
+										printf " --no-track-tags --no-chapters --language 0:${MuxLang} ( "${outputdir}/${output}.a${mapindex}.m4a" )"
+									done)
+		cmd_mux_tail='--track-order 0:0'$(for ((mapindex=0; mapindex<${AudioNum}; ++mapindex))
+									do
+										printf ','$((${mapindex}+1))':0'
+									done)
+
+		if [ ${SubNum} -gt 0 ]; then
+			if [ -f "${outputdir}/${output}.subtmp.mkv" ]; then
+				rm -f "${outputdir}/${output}.subtmp.mkv"
 			fi
-			if [ ! -f "$outputdir/$output.sub.mkv" ]; then
+			if [ ! -f "${outputdir}/${output}.sub.mkv" ]; then
+				#echo "$(date +"%Y.%m.%d %H:%M:%S") Subtitle Stream(s) Detected, Start of Subtitle Copying..." | tee -a ${LOGFILE}
 				ffmpeg-10bit -hide_banner \
 					-v quiet \
-					-i "$input" \
+					-i "${input}" \
 					-map 0 \
 					-map_chapters -1 \
 					-an -vn \
 					-scodec copy \
-					"$outputdir/$output.subtmp.mkv"&&mv "$outputdir/$output.subtmp.mkv" "$outputdir/$output.sub.mkv"
+					"${outputdir}/${output}.subtmp.mkv"&&mv "${outputdir}/${output}.subtmp.mkv" "${outputdir}/${output}.sub.mkv"
 			fi
-			if [ ! -f "$outputdir/$output.$tail.mkv" ]; then
+			if [ ! -f "${outputdir}/${output}.${tail}.mkv" ]; then
+				#echo "$(date +"%Y.%m.%d %H:%M:%S") Start of Final Output File Muxing..." | tee -a ${LOGFILE}
 				mkvmerge \
-					--output "$outputdir/$output.tail.mkv" \
+					--output "${outputdir}/${output}.tail.mkv" \
 					--no-audio \
 					--no-subtitles \
 					--no-track-tags \
 					--no-global-tags \
-					--language 0:$MuxLang \
+					--language 0:${MuxLang} \
 					--default-track 0:yes \
-					--aspect-ratio 0:$dar \
+					--aspect-ratio 0:${dar} \
 					--track-name 0:"" \
-					'(' $outputdir/$output.done.mp4 ')' \
-					$cmd_mux_audio \
+					\(  "${outputdir}/${output}.done.mp4" \) \
+					${cmd_mux_audio} \
 					--no-chapters \
 					--no-audio \
 					--no-video \
 					--no-track-tags \
 					--no-global-tags \
-					'(' $outputdir/$output.sub.mkv ')' \
+					\(  "${outputdir}/${output}.sub.mkv" \) \
 					--title "" \
-					$cmd_mux_tail&&mv "$outputdir/$output.tail.mkv" "$outputdir/$output.$tail.mkv"
-				if [ -f "$outputdir/$output.$tail.mkv" ]; then
-					rm -f "$outputdir/$output.done.mp4"
-					rm -f "$outputdir/$output.sub.mkv"
-					for ((mapindex=0; mapindex<$(ffmpeg-10bit -i "$input" 2>&1 | grep "Audio" | wc -l); ++mapindex))
+					${cmd_mux_tail}&&mv "${outputdir}/${output}.tail.mkv" "${outputdir}/${output}.${tail}.mkv"
+				if [ -f "${outputdir}/${output}.${tail}.mkv" ]; then
+					rm -f "${outputdir}/${output}.done.mp4"
+					rm -f "${outputdir}/${output}.sub.mkv"
+					for ((mapindex=0; mapindex<${AudioNum}; ++mapindex))
 					do
-						rm -f "$outputdir/$output.a$mapindex.m4a"
+						rm -f "${outputdir}/${output}.a${mapindex}.m4a"
 					done
 				fi
 			fi
 		else
-			if [ ! -f "$outputdir/$output.$tail.mkv" ]; then
+			if [ ! -f "${outputdir}/${output}.${tail}.mkv" ]; then
+				#echo "$(date +"%Y.%m.%d %H:%M:%S") Start of Final Output File Muxing..." | tee -a ${LOGFILE}
 				mkvmerge \
-					--output "$outputdir/$output.tail.mkv" \
+					--output "${outputdir}/${output}.tail.mkv" \
 					--no-audio \
 					--no-subtitles \
 					--no-track-tags \
 					--no-global-tags \
-					--language 0:$MuxLang \
+					--language 0:${MuxLang} \
 					--default-track 0:yes \
-					--aspect-ratio 0:$dar \
+					--aspect-ratio 0:${dar} \
 					--track-name 0:"" \
-					'(' $outputdir/$output.done.mp4 ')' \
-					$cmd_mux_audio \
+					\(  "${outputdir}/${output}.done.mp4" \) \
+					${cmd_mux_audio} \
 					--title "" \
-					$cmd_mux_tail&&mv "$outputdir/$output.tail.mkv" "$outputdir/$output.$tail.mkv"
-				if [ -f "$outputdir/$output.$tail.mkv" ]; then
-					rm -f "$outputdir/$output.done.mp4"
-					for ((mapindex=0; mapindex<$(ffmpeg-10bit -i "$input" 2>&1 | grep "Audio" | wc -l); ++mapindex))
+					${cmd_mux_tail}&&mv "${outputdir}/${output}.tail.mkv" "${outputdir}/${output}.${tail}.mkv"
+				if [ -f "${outputdir}/${output}.${tail}.mkv" ]; then
+					rm -f "${outputdir}/${output}.done.mp4"
+					for ((mapindex=0; mapindex<${AudioNum}; ++mapindex))
 					do
-						rm -f "$outputdir/$output.a$mapindex.m4a"
+						rm -f "${outputdir}/${output}.a${mapindex}.m4a"
 					done
 				fi
 			fi
 		fi
+		#echo "$(date +"%Y.%m.%d %H:%M:%S") End of Encoding..." | tee -a ${LOGFILE}
 	fi
 }
 
